87085675ac96787e1d44e353854ba1e16ff915e2
HikariZasureiya <info.d.podder@gmail.com>
2025-06-29 01:24:23 +0530

    shit just worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState , useRef, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Click from './components/Transmitter.jsx'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "../src/pages/home"
 
 
 function App() {
-  
-  const display = useRef();
-  const [strii , setStrii] = useState('');
-  const [textstr , SetTextStr] = useState('');
-
   return (
-    <div className="flex">
-     <Click
-      setStrii={setStrii}
-      SetTextStr = {SetTextStr}
-     />
-
-     <div ref = {display} 
-      style={{marginBottom:'20px' , marginTop:'10px'}}
-     >
-      Morse: {strii}
-     </div>
-
-     <div >
-      Text: {textstr}
-     </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
+
+
+
--- src/components/Transmitter.jsx
+++ src/components/Transmitter.jsx
@@ -1,206 +1,178 @@
-import { useState , useRef, useEffect } from 'react'
-import morsetotext  from '../assets/morsetotext.json'
-import  texttomorse  from '../assets/texttomorse.json'
+import { useState, useRef, useEffect } from "react";
+import morsetotext from "../assets/morsetotext.json";
+import texttomorse from "../assets/texttomorse.json";
 
-  
-
-export default function({setStrii , SetTextStr}){
-  
-  const [ismobile , setIsMobile] = useState(false);
-  const [clicked , SetClicked] = useState(0);
-  const [unclicked , setUnclicked] = useState(0);
-  const [string , SetString] = useState('');
-  const [mstring , SetmString] = useState('');
-  const [downevent , setDownevent] = useState(null);
-  const [upevent , setUpevent] = useState(null);
-  const [temp , setTemp] = useState('');
-  const [mtemp , setmtemp] = useState('');
+export default function ({ setStrii, SetTextStr }) {
+  const [ismobile, setIsMobile] = useState(false);
+  const [clicked, SetClicked] = useState(0);
+  const [unclicked, setUnclicked] = useState(0);
+  const [string, SetString] = useState("");
+  const [mstring, SetmString] = useState("");
+  const [downevent, setDownevent] = useState(null);
+  const [upevent, setUpevent] = useState(null);
+  const [temp, setTemp] = useState("");
+  const [mtemp, setmtemp] = useState("");
   const clickedRef = useRef(clicked);
   const unclickedRef = useRef(unclicked);
   const mtempref = useRef(mtemp);
 
   useEffect(() => {
-    const checkIfMobile = /Mobi|Android|iPhone|iPad|iPod|Windows Phone|webOS|BlackBerry|IEMobile/i.test(navigator.userAgent);
+    const checkIfMobile =
+      /Mobi|Android|iPhone|iPad|iPod|Windows Phone|webOS|BlackBerry|IEMobile/i.test(
+        navigator.userAgent
+      );
     setIsMobile(checkIfMobile);
   }, []);
 
-// store the mousedown time in a reference using useEffect to avoid bugs ;( 
+  // store the mousedown time in a reference using useEffect to avoid bugs ;(
   useEffect(() => {
     clickedRef.current = clicked;
   }, [clicked]);
 
-// same as above but for unclicked
+  // same as above but for unclicked
   useEffect(() => {
     unclickedRef.current = unclicked;
   }, [unclicked]);
 
-//   useEffect(() => {
-//     tempref.current = temp;
-//   }, [temp]);
-
+  //   useEffect(() => {
+  //     tempref.current = temp;
+  //   }, [temp]);
 
   // update Strii as needed
-  useEffect(()=>{
+  useEffect(() => {
     setStrii(string);
-  },[string])
+  }, [string]);
 
-  useEffect(()=>{
- mtempref.current = mtemp;
-  },[mtemp])
+  useEffect(() => {
+    mtempref.current = mtemp;
+  }, [mtemp]);
 
-  useEffect(()=>{
+  useEffect(() => {
+    SetTextStr((prev) => {
+      if (morsetotext.hasOwnProperty(temp)) return mstring + morsetotext[temp];
+      else if (texttomorse.hasOwnProperty(temp)) return mstring + temp;
+      else return mstring;
+    });
+  }, [mstring, temp]);
 
-    
-    SetTextStr((prev)=>{
-        if(morsetotext.hasOwnProperty(temp))
-            return mstring+morsetotext[temp];
-        else if(texttomorse.hasOwnProperty(temp))
-            return mstring+temp;
-        else
-            return mstring;
-    }
-    );
-
-  },
-  [mstring , temp])
-
- 
   // typein checks for unclicked value to seperate letters annd words
-  const typein = ()=>{
-        
-        if (upevent){
-          clearInterval(upevent);
-          setUpevent(null);
-        }
-        if (unclickedRef.current >= 560){
-         SetString((prev) => prev+'/');
-            SetmString((prev) => {
-
-                if(texttomorse.hasOwnProperty(temp))
-                    return prev+temp+" ";
-                else if(morsetotext.hasOwnProperty(temp))
-                    return prev+morsetotext[temp]+" ";
-                else
-                    return prev+" ";
-          
-         });
-         setTemp(''); 
-         setmtemp('');
-
-        }
-        else if (unclickedRef.current >= 240){
-          SetString(prev => prev+' '); 
-          
-          SetmString((prev) => {
-
-                if(texttomorse.hasOwnProperty(temp))
-                    return prev+temp;
-                else if(morsetotext.hasOwnProperty(temp))
-                    return prev+morsetotext[temp];
-                else 
-                    return prev;
-          });
-          setTemp('');
-          setmtemp('');
-         }
-        setUnclicked(0);
+  const typein = () => {
+    if (upevent) {
+      clearInterval(upevent);
+      setUpevent(null);
+    }
+    if (unclickedRef.current >= 560) {
+      SetString((prev) => prev + "/");
+      SetmString((prev) => {
+        if (texttomorse.hasOwnProperty(temp)) return prev + temp + " ";
+        else if (morsetotext.hasOwnProperty(temp))
+          return prev + morsetotext[temp] + " ";
+        else return prev + " ";
+      });
+      setTemp("");
+      setmtemp("");
+    } else if (unclickedRef.current >= 240) {
+      SetString((prev) => prev + " ");
+      SetmString((prev) => {
+        if (texttomorse.hasOwnProperty(temp)) return prev + temp;
+        else if (morsetotext.hasOwnProperty(temp))
+          return prev + morsetotext[temp];
+        else return prev;
+      });
+      setTemp("");
+      setmtemp("");
+    }
+    setUnclicked(0);
 
     // set interval to check for clickdown time spent
     const down = setInterval(() => {
-      SetClicked(prev => prev+10);
+      SetClicked((prev) => prev + 10);
     }, 10);
     setDownevent(down);
-  }
+  };
 
   // works same as typein but adds the main dits and dahs to the string
-  const typeout = ()=>{
-    if(downevent){
-        clearInterval(downevent);
-        if(clickedRef.current >= 240){
-          SetString(prev => prev+'-');
-          setTemp((prev)=>{
-            if(morsetotext.hasOwnProperty(texttomorse[prev]+'-')){
-                    return morsetotext[texttomorse[prev]+'-'];
-            }
-            else
-                return mtempref.current+'-';
-          })
-          setmtemp((prev)=> prev+'-');
-
-        }
-        else{
-          SetString(prev => prev+'.');
-          setTemp((prev)=>{
-            if(morsetotext.hasOwnProperty(texttomorse[prev]+'.')){
-                    return morsetotext[texttomorse[prev]+'.'];
-            }
-            else
-                return mtempref.current+'.';
-
-          })
-
-          setmtemp((prev)=> prev+'.');
-          
-        }
-        SetClicked(0);
+  const typeout = () => {
+    if (downevent) {
+      clearInterval(downevent);
+      if (clickedRef.current >= 240) {
+        SetString((prev) => prev + "-");
+        setTemp((prev) => {
+          if (morsetotext.hasOwnProperty(texttomorse[prev] + "-")) {
+            return morsetotext[texttomorse[prev] + "-"];
+          } else return mtempref.current + "-";
+        });
+        setmtemp((prev) => prev + "-");
+      } else {
+        SetString((prev) => prev + ".");
+        setTemp((prev) => {
+          if (morsetotext.hasOwnProperty(texttomorse[prev] + ".")) {
+            return morsetotext[texttomorse[prev] + "."];
+          } else return mtempref.current + ".";
+        });
+        setmtemp((prev) => prev + ".");
       }
+      SetClicked(0);
+    }
 
-  
-    
     const up = setInterval(() => {
-      setUnclicked((prev) => prev+10);
+      setUnclicked((prev) => prev + 10);
 
       // clears string after 3 seconds of inactivity
-      if(unclickedRef.current >= 3000){
+      if (unclickedRef.current >= 3000) {
         clearInterval(up);
         setUpevent(null);
         setUnclicked(0);
-        SetString('');
-        setTemp('');
-        SetmString('');
-        setmtemp('');
+        SetString("");
+        setTemp("");
+        SetmString("");
+        setmtemp("");
       }
-
     }, 10);
 
     setUpevent(up);
-    
-  }
-
-
-  return(<>
-
-  <div>
-    {ismobile ? (
-
-      <button
-      style={{backgroundColor:'#EE4B2B' , width:'100px', height:'50px'}}
-      onTouchStart={(e)=>{
-        typein();
-        e.target.style.backgroundColor='#E97451'
-      }} 
-        onTouchEnd={(e)=>{
-            typeout();
-            e.target.style.backgroundColor='#EE4B2B'
-        }}>MOB</button>
-    ):
-  (
-      <button
-      style={{backgroundColor:'#EE4B2B' , width:'100px', height:'50px'}}
-      onMouseDown={(e)=>{
-        typein();
-        e.target.style.backgroundColor='#E97451'
-      }} 
-        onMouseUp={(e)=>{
-            typeout();
-            e.target.style.backgroundColor='#EE4B2B'
-        }}>PC</button>
-  )}
-  </div>
- 
-
-  </>)
-
-
+  };
+  return (
+    <>
+      <div>
+        {ismobile ? (
+          <button
+            style={{
+              backgroundColor: "#EE4B2B",
+              width: "100px",
+              height: "50px",
+            }}
+            onTouchStart={(e) => {
+              typein();
+              e.target.style.backgroundColor = "#E97451";
+            }}
+            onTouchEnd={(e) => {
+              typeout();
+              e.target.style.backgroundColor = "#EE4B2B";
+            }}
+          >
+            MOB
+          </button>
+        ) : (
+          <button
+            style={{
+              backgroundColor: "#EE4B2B",
+              width: "100px",
+              height: "50px",
+            }}
+            onMouseDown={(e) => {
+              typein();
+              e.target.style.backgroundColor = "#E97451";
+            }}
+            onMouseUp={(e) => {
+              typeout();
+              e.target.style.backgroundColor = "#EE4B2B";
+            }}
+          >
+            PC
+          </button>
+        )}
+      </div>
+    </>
+  );
 }
-
